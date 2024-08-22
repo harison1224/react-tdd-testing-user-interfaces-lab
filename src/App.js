d863a3e960292723f17d07cf5e530b28b926af3c
harison1224 <harisonochieng0@gmail.com>
2024-08-22 11:15:53 +0300

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+// function App() {
+//   return <div>Your code here</div>;
+// }
+
+// export default App;
 function App() {
-  return <div>Your code here</div>;
+  return <div>
+    <h1>Hi, I'm harison1224</h1>
+    <h2>About Me</h2>
+    <p>I am pursuing a career in Software Development. I am very passionate about the work and am eager to learn as much as I can</p>
+    
+   <div> <img src="https://curriculum-content.s3.amazonaws.com/phase-2/react-tdd-testing-user-interfaces-lab/testing-user-interfaces-lab.png" alt="Picture" />
+    </div>
+    <a name="linkedIn" href="">LinkedIn</a>
+    <div></div>
+    <a name="github" href="https://github.com/harison1224/react-tdd-testing-user-interfaces-lab.git">GitHub</a>
+  </div>;
 }
 
 export default App;
